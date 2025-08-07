c15d085d9c678867bacf27649b4cb690719d7762
MurilojrMarques <muriloj2702@gmail.com>
2025-08-07 16:42:29 -0300

    Implement CreateUser method and improve error message consistency in user store

--- service/user/store.go
+++ service/user/store.go
@@ -15,6 +15,15 @@ func NewStore(db *sql.DB) *Store {
 	return &Store{db: db}
 }
 
+func (s *Store) CreateUser(user types.User) error {
+	_, err := s.db.Exec("INSERT INTO users (firstName, lastName, email, password) VALUES (?, ?, ?, ?)", user.FirstName, user.LastName, user.Email, user.Password)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (s *Store) GetUserByEmail(email string) (*types.User, error) {
 	rows, err := s.db.Query("SELECT * FROM users WHERE email = ?", email)
 	if err != nil {
@@ -30,7 +39,28 @@ func (s *Store) GetUserByEmail(email string) (*types.User, error) {
 	}
 
 	if u.ID == 0 {
-		return nil, fmt.Errorf("Usuário não encontrado")
+		return nil, fmt.Errorf("usuário não encontrado")
+	}
+
+	return u, nil
+}
+
+func (s *Store) GetUserByID(id int) (*types.User, error) {
+	rows, err := s.db.Query("SELECT * FROM users WHERE id = ?", id)
+	if err != nil {
+		return nil, err
+	}
+
+	u := new(types.User)
+	for rows.Next() {
+		u, err = scanRowsIntoUser(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if u.ID == 0 {
+		return nil, fmt.Errorf("usuário não encontrado")
 	}
 
 	return u, nil
